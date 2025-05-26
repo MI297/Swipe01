144dcce953962f9f7199ddb8f801181a7102163e
MI297 <ichigima01@gmail.com>
2025-05-26 09:56:00 +0900

    # Swipe01 ソース分割　
    ## 現状実装されている内容
    - スワイプに応じて画面の線のねじれがアニメーションを行い、大きくなる。
    - カウントが画面上部に表示される（最大1000）
    ‐リセットボタン
    -背景画面

--- app/src/main/java/com/example/swipe01/TwistedLineCanvas.kt
+++ app/src/main/java/com/example/swipe01/TwistedLineCanvas.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
